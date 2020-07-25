b613b70a074ad4b9d262806ca24dbb20687aeca4
Benedikt242 <b242@posteo.de>
2020-07-25 17:35:30 +0200

    added split

--- 2test/regexp.py
+++ 2test/regexp.py
@@ -5,6 +5,9 @@ import re
 regex = re.compile("Ban")
 with open("../data/fail2ban.all.jatos") as f:
     for line in f:
+        line = line.rstrip()
         result = regex.search(line)
         if result:
-            print(line)
+            arrg = line.split()
+            print(arrg[0]+" "+arrg[7])
+            exit(0)

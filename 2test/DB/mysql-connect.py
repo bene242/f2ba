bc9c9040c950234e53da97ac6d3ff3c5fdd23056
bene242 <b242@posteo.de>
2020-09-06 16:48:40 +0200

    DB aktualisiert

--- 2test/DB/mysql-connect.py
+++ 2test/DB/mysql-connect.py
@@ -1,6 +1,33 @@
 import mysql.connector
+import argparse
+import csv
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help="db config file")
+args = parser.parse_args()
+
+mhost = ""
+muser = ""
+mpassword = ""
+mdatabase = ""
+
+if args.file:
+    with open(args.file, newline='') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            #print(row)
+            
+            mhost = row['host']
+            muser = row['user']
+            mpassword = row['password']
+            mdatabase = row['database']
+            if True:
+                break
 
 mydb = mysql.connector.connect(
-
+    host=mhost,
+    user=muser,
+    password=mpassword,
+    database=mdatabase
 )
 print(mydb)

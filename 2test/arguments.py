7b46863c13e763d2a2ab805b86e48eaec32d2cd7
bene242 <b242@posteo.de>
2020-09-06 11:35:33 +0200

    alte Datei gelöscht

--- 2test/arguments.py
+++
@@ -1,6 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("square", help="square the number entered in this pos (positional argument)", type=int)
-args = parser.parse_args()
-#print(args.echo)
-print(args.square**2)
